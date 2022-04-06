52b08e8f85deb965110f025743ea226824198d11
karduonggamin <100028167+karduonggamin@users.noreply.github.com>
2022-04-06 22:59:12 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
     titleWeb: "demo",
-    introTitle: 'Phạm Thị Phương Thúy ',
+    introTitle: 'Ngô Phương Thảo  ',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
